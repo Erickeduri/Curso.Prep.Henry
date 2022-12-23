317ef78690ec85030900d612bb84096371a828aa
Erick Rivero Eduardo Alcala <eera2409@gmail.com>
2022-12-23 15:03:27 -0300

    js5 resuelto

--- 05-JS-IV/homework/homework.js
+++ 05-JS-IV/homework/homework.js
@@ -155,11 +155,6 @@ function agregarMetodoCalculoDescuento(producto) {
   // producto.porcentajeDeDescuento -> 0.2 (o simplemente ".2")
   // producto.calcularPrecioDescuento() -> 20 - (20 * 0.2)
   // Tu código:
-  
- // producto.calcularPrecioDescuento = function (){
- //return this.precio  - ( this.precio * this.porcentajeDeDescuento );
-  //}
-  //return producto;
 
   producto.calcularPrecioDescuento = function (){
    return this.precio  - (this.precio * this.porcentajeDeDescuento );S
